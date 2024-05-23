617763ffa88dfdaf74cd05a35bc49afdeb72c78e
Evelyn Gaitan <143912123+evelyngaitan92@users.noreply.github.com>
2024-05-22 22:23:45 -0300

    formato

--- validacion.js
+++ validacion.js
@@ -1,15 +1,15 @@
 const formulario = document.getElementById('formulario');
 const elementos = document.querySelectorAll('#formulario [name]');
 const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+  email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
 
 const validarFormulario = (elem) => {
   switch (elem.name) {
     case "name":
     case "surname":
-      if (expresiones.nombre.test(elem.value)) { 
+      if (expresiones.nombre.test(elem.value)) {
         elem.classList.remove('error');
         return true;
       } else {
@@ -23,7 +23,7 @@ const validarFormulario = (elem) => {
       } else {
         elem.classList.add('error');
         return false;
-      } 
+      }
     case "select":
     case "comentario":
     case "imagen":
@@ -31,21 +31,21 @@ const validarFormulario = (elem) => {
   return true;
 }
 
-elementos.forEach((elem) =>{
+elementos.forEach((elem) => {
   elem.addEventListener('keyup', (e) => validarFormulario(e.target)) // key tecla levantada Key up 
   elem.addEventListener('blur', (e) => validarFormulario(e.target))
 })// clik por fuera 
 
-     
+
 formulario.addEventListener('submit', (e/*evento*/) => {
- e.preventDefault();
- console.log('submitting...');
+  e.preventDefault();
+  console.log('submitting...');
 
-let validado = true;
+  let validado = true;
 
- for(const elem of elementos){
+  for (const elem of elementos) {
     let resultado = validarFormulario(elem); // true = ok - false = error 
-    if (resultado == false){
+    if (resultado == false) {
       validado = false;
     }
   }
